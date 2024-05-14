037d9cf7defe5c066a1ce57d2c487fc20db090d8
Duncan <duncan.deane@konghq.com>
2024-05-14 14:44:58 +0100

    multip-platform build

--- docker-build-multi-platform.sh
+++ docker-build-multi-platform.sh
@@ -1,6 +1,6 @@
 # Set Docker Hub username
 DOCKER_HUB_USERNAME="kongedu"
-TAG="2.0"
+TAG="1.0"
 
 docker buildx create --use
 
@@ -16,7 +16,7 @@ docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_HUB_USERNAME/k
 docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_HUB_USERNAME/kongair-customers:$TAG -f ./sales/customer/Dockerfile ./sales/customer --push
 
 # flight-data/flights
-docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_HUB_USERNAME/kongair-flights:$TAG-f ./flight-data/flights/Dockerfile ./flight-data/flights --push
+docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_HUB_USERNAME/kongair-flights:$TAG -f ./flight-data/flights/Dockerfile ./flight-data/flights --push
 
 # flight-data/routes
 docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_HUB_USERNAME/kongair-routes:$TAG -f ./flight-data/routes/Dockerfile ./flight-data/routes --push
